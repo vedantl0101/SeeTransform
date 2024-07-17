17881a6a21a13d5c4b30339ca3373b3c4fbefab1
Vedant Lokhande <vedantlokhande0101@gmail.com>
2024-07-17 13:26:45 +0530

    final commit

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -12,6 +12,7 @@ const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps)
   if(!userId) redirect('/sign-in')
 
   const user = await getUserById(userId);
+  
 
   return (
     <>
